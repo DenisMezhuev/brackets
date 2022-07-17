4f17fff4de6222c7a222ac91455a38b92a736383
Denis Ermakov <alexmezhuev@yandex.by>
2022-07-17 09:48:01 +0300

    init: performed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+ 
+  let string = str;
+  for(let i = string.length; i >= 0; i--){
+       bracketsConfig.forEach(elem =>{
+            let bracket = elem.join('');
+           if(string.includes(bracket)){
+           string = string.replace(bracket, '');
+           }
+           
+       });
+      
+    
+  }
+       return string === '';
+
 }
